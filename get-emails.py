f0459afc674c7aa6d8052a7148b7678351bad7d1
Jorge Isnardo Altamirano <jisnardo88@gmail.com>
2015-06-19 17:17:44 +0200

    mv to get-emails.py

--- get-emails.py
+++ get-emails.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+'''
+
 if __name__ == '__main__':
     
     f_sql, f_txt = open('user_data_only.sql','r'), open('mail_list.txt','w')
